48b30a014879bc3195af4e2ebdb6dc0e151cbce2
Ricardo Acuna <69739397+MrRicardoAcuna7@users.noreply.github.com>
2023-04-15 17:25:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Hard-Boiled Free-Range Egg')
