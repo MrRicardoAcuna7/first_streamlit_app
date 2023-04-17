d29200156daeb051ef40a4feed2f9d7e31d6a3ef
Ricardo Acuna <69739397+MrRicardoAcuna7@users.noreply.github.com>
2023-04-17 15:14:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
-streamlit.text("What fruit do you like to add?")
-streamlit.write('The user entered ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit do you like to add?','Kiwi')
 
